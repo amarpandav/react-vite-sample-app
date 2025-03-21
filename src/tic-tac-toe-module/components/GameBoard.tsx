7e60e34fab403d044c03c90fc161e8be014c9360
amarpandav <amarpandav@gmail.com>
2025-03-21 12:32:40 +0100

    continue ttt game

--- src/tic-tac-toe-module/components/GameBoard.tsx
+++ src/tic-tac-toe-module/components/GameBoard.tsx
@@ -1,22 +1,34 @@
 import classes from "../pages/TTTPage.module.css";
+import {useState} from "react";
 
 export default function GameBoard() {
 
-    const initialGameBoard = [
+    const initialGameBoard: (null | string)[][] = [
         [null, null, null],
         [null, null, null],
         [null, null, null],
     ];
 
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+
+    function handleSelectSquare(rowIndex: number, colIndex: number) {
+        setGameBoard( (prevGameBoard) => {
+            const newGameBoard = [...prevGameBoard].map( (innerArray) => [...innerArray]);
+            newGameBoard[rowIndex][colIndex] = 'X';
+            return newGameBoard;
+        });
+    }
+
     return (
         <ol id="game-board" className={classes.gameBoard}>
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {
                             row.map( (col, colIndex) => (
                                 <li key={colIndex}>
-                                    <button>{col}</button>
+                                    <button onClick={ ()=> handleSelectSquare(rowIndex, colIndex)}>{col}</button>
                                     {/*col is player sombol*/}
                                 </li>
                             ))
