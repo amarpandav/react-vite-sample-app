378d8e6edd06d6da463d469affbaac20ae7b792b
amarpandav <amarpandav@gmail.com>
2025-03-23 09:53:19 +0100

    continue ttt game

--- src/tic-tac-toe-module/pages/TTTPage.tsx
+++ src/tic-tac-toe-module/pages/TTTPage.tsx
@@ -2,7 +2,7 @@ import Player from "../components/Player.tsx";
 import classes from "./TTTPage.module.css";
 //import PlayerTemp from "../components/PlayerTemp.tsx";
 import GameBoard from "../components/GameBoard.tsx";
-import {useMemo, useState} from "react";
+import {/*useMemo, */useState} from "react";
 import Log from "../components/Log.tsx";
 import {SquareDto, TurnDto} from "./TurnDto.ts";
 
@@ -11,13 +11,22 @@ export default function TTTPage() {
     const [turns, setTurns] = useState<TurnDto[]>([]);
 
     //const [activePlayerSymbol, setActivePlayerSymbol] = useState('X'); no need to store activePlayerSymbol, recalculating it using useMemo.turns
-    const activePlayerSymbol = useMemo( () => {
+    //Option 1: use useMemo to calculate activePlayerSymbol
+    /*const activePlayerSymbol = useMemo( () => {
         if (turns.length === 0) {
             return 'X';
         }
         return turns[0].activePlayerSymbol === 'X' ? 'O' : 'X';
-    }, [turns]);
+    }, [turns]);*/
 
+    //Option 2: create a function to calculate activePlayerSymbol
+    const deriveActivePlayerSymbol = (gameTurns: TurnDto[]) => {
+        let activePlayerSymbol = 'X';
+        if (gameTurns.length > 0 && gameTurns[0].activePlayerSymbol === 'X') {
+            activePlayerSymbol = 'O'
+        }
+        return activePlayerSymbol;
+    }
 
     function handleSelectSquare(rowIndex: number, colIndex: number) {
         //step 1. toggle the active player symbol
@@ -28,7 +37,7 @@ export default function TTTPage() {
         //States are immutable, so we can't merge them. We can only replace them.
         //setTurns( (prevTunrs/*: {activePlayerSymbol: string, square: {rowIndex: number, colIndex: number}}[]*/) => {
         setTurns((prevTunrs: TurnDto[]) => {
-
+            const activePlayerSymbol = deriveActivePlayerSymbol(prevTunrs);
             //activePlayerSymbol would have current active player however if we use that then we are merging two states here.
             //We know react is hacky, it will batch the state updates and then apply them. So, we can't rely on the activePlayerSymbol.
             //Hence redo the logic to get the active player symbol
@@ -52,9 +61,13 @@ export default function TTTPage() {
         <>
             <div id="game-container" className={classes.gameContainer}>
                 <ol id="players" className={`${classes.players} ${classes.highlightPlayer}`}>
+                    {/*
                     <Player initialPlayerName="Rian" playerSymbol="X" isActive={activePlayerSymbol === 'X'}/>
                     <Player initialPlayerName="Amar" playerSymbol="O" isActive={activePlayerSymbol === 'O'}/>
+                    */}
 
+                    <Player initialPlayerName="Rian" playerSymbol="X" isActive={deriveActivePlayerSymbol(turns) === 'X'}/>
+                    <Player initialPlayerName="Amar" playerSymbol="O" isActive={deriveActivePlayerSymbol(turns) === 'O'}/>
                     {/*<PlayerTemp />*/}
                 </ol>
                 <GameBoard callback={handleSelectSquare} turns={turns}/>
