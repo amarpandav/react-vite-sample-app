229b11ce4a828c6f8ef94e69eee8ca29bcf53efb
amarpandav <amarpandav@gmail.com>
2025-03-29 11:11:57 +0100

    investment calculator module

--- src/investModule/pages/InvestHomePage.tsx
+++ src/investModule/pages/InvestHomePage.tsx
@@ -1,15 +1,15 @@
 //import PlayerTemp from "../components/PlayerTemp.tsx";
 
 import Header from "../components/Header/Header.tsx";
+import UserInput from "../components/UserInput/UserInput.tsx";
 
 export default function InvestHomePage() {
 
 
     return (
         <>
-            <Header>
-
-            </Header>
+            <Header></Header>
+            <UserInput></UserInput>
         </>
     );
 }
