bb12e24edd1bc570a1273c7aae379ce724cb4a16
amarpandav <amarpandav@gmail.com>
2025-03-31 18:36:54 +0200

    RefsAndPortalsHomePage

--- src/refsAndPortalsModule/components/ResultModal/ResultModal.tsx
+++ src/refsAndPortalsModule/components/ResultModal/ResultModal.tsx
@@ -1,22 +1,37 @@
 import classes from "./ResultModal.module.css";
+//import {useImperativeHandle} from "react";
 
 interface Props {
     ref: React.RefObject<HTMLDialogElement>,
     result: string;
     targetTime: number;
 }
-export default function ResultModal({ref, result, targetTime}: Props) {
+
+export default function ResultModal({ref, result, targetTime}: Props/*, ref2: React.Ref<HTMLDialogElement>*/) {
+
+    /*YOu need to use forwardRef to get it working :
+useImperativeHandle() is used to decouple this components and its insights to the parent component. so ourside would call open() but internally we can call
+whatever we want like showModal here.
+ We can replace <dialog> with <div> and it will work as expected.
+
+    useImperativeHandle(ref2, () => {
+        return {
+            open() {
+                ref.current?.showModal();
+            }
+        }
+    });*/
 
     return (
-    <dialog ref={ref} className={classes.resultModal}>
-      <h2>You {result}</h2>
-        <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+        <dialog ref={ref} className={classes.resultModal}>
+            <h2>You {result}</h2>
+            <p>The target time was <strong>{targetTime}</strong> seconds.</p>
 
-        <p>You stopped the timer with <strong>X seconds left</strong> seconds.</p>
-        <form method="dialog">
-            <button>Close</button>
-        </form>
+            <p>You stopped the timer with <strong>X seconds left</strong> seconds.</p>
+            <form method="dialog">
+                <button>Close</button>
+            </form>
 
-    </dialog>
-  );
+        </dialog>
+    );
 }
